832b7a8426d904d68b68fcf6a5c8a108761aacc8
Ealona Shmoel <ealona.shmoel@gmail.com>
2017-06-17 22:48:24 -0400

    Fix property incorrect spelling

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -17,9 +17,9 @@ app.get('/', function(req, res) {
 app.get('/users', function(req, res) {
   // Hard coding for simplicity. Pretend this hits a real db
   res.json([
-    {"id": 1, "firstname": "Bob", "lastname": "Smith", "email": "bob@gmail.com"},
-    {"id": 2, "firstname": "Tammy", "lastname": "Norton", "email": "tnorton@yahoo.com"},
-    {"id": 3, "firstname": "Tina", "lastname": "Lee", "email": "lee.tina@hotmail.com"}
+    {"id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com"},
+    {"id": 2, "firstName": "Tammy", "lastName": "Norton", "email": "tnorton@yahoo.com"},
+    {"id": 3, "firstName": "Tina", "lastName": "Lee", "email": "lee.tina@hotmail.com"}
   ]);
 });
 
