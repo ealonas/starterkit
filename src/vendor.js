a5c0a77476f97638f941c49ee588ec886b897b53
Ealona Shmoel <ealona.shmoel@gmail.com>
2017-06-17 22:34:37 -0400

    Add bundle splitting example

---
+++ src/vendor.js
@@ -0,0 +1,15 @@
+/**
+ * This file contains references to the vendor libraries we are using in this project.
+ * This is used by webpack in the production build only. A separate bundle for
+ * vendor code is
+ * useful since it is unlikely to change as often as the application code.
+ * So all libraries will
+ * be referenced here so that they can be cached by the user until one of
+ * the libraries change.
+ * This saves the user having to redownload a huge file everytime one
+ * line of code changes.
+ */
+
+/* eslint-disable no-unused-vars */
+
+import fetch from 'whatwg-fetch';
