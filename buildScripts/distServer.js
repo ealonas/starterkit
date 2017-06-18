c89347aa0e86bafc7ab4ab2883d6f60909dc8eb9
Ealona Shmoel <ealona.shmoel@gmail.com>
2017-06-17 22:05:52 -0400

    Add build script for prod, includes minification and dynamic html generation

---
+++ buildScripts/distServer.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import compression from 'compression';
+
+/* eslint-disable no-console */
+const port = 3000;
+const app = express();
+
+app.use(compression());
+app.use(express.static('dist'));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
+app.get('/users', function(req, res) {
+  // Hard coding for simplicity. Pretend this hits a real db
+  res.json([
+    {"id": 1, "firstname": "Bob", "lastname": "Smith", "email": "bob@gmail.com"},
+    {"id": 2, "firstname": "Tammy", "lastname": "Norton", "email": "tnorton@yahoo.com"},
+    {"id": 3, "firstname": "Tina", "lastname": "Lee", "email": "lee.tina@hotmail.com"}
+  ]);
+});
+
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    open('http://localhost:' + port);
+  }
+});
